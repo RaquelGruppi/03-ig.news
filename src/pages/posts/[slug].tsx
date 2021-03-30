c788616587697607dd728ed8313b1d25742b3f42
Raquel Gruppi <raquel.gruppi@gmail.com>
2021-03-30 20:16:50 +1100

    Add subscription validation

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -37,7 +37,17 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const session = await getSession();
+  const session = await getSession({ req });
+
+  if(!session?.activeSubscription) {
+    return {
+      redirect:{
+        destination: '/',
+        permanent: false
+      }   
+    }
+  }
+
   const { slug } = params;
   
   const prismic = getPrismicClient(req);
